9106cc6900ec8bfd799956bddb0470971f7ff0d0
Kodeneko <ladysun.freedom@gmail.com>
2026-08-08 01:29:42 +0200

    [storybook]: Created Badge component

---
+++ src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import '../../styles/global.css';
+import Badge from "./Badge";
+
+const meta: Meta<typeof Badge> = {
+  title: "Components/Badge",
+  component: Badge,
+  tags: ["autodocs"],
+};
+
+
+export default meta;
+
+
+type Story = StoryObj<typeof Badge>;
+
+
+export const Positive: Story = {
+  args:{
+    val: 6
+  }
+};
+
---
+++ src/components/Badge/Badge.types.ts
@@ -0,0 +1,5 @@
+type BadgeProps = {
+  val: number
+}
+
+export type { BadgeProps };
