0186412a31774905cb4490547950a9b8b2dcefb3
Kodeneko <ladysun.freedom@gmail.com>
2026-08-08 00:57:15 +0200

    [storybook]: Included Fontawesome

---
+++ src/icons/icons.ts
@@ -0,0 +1,45 @@
+import {
+  faBoxOpen,
+  faSackDollar,
+  faShirt,
+  faUserGroup,
+  faTableCellsLarge,
+  faFileLines,
+  faShop,
+  faMagnifyingGlass,
+  faMoon,
+  faSun,
+  faBell,
+  faRightFromBracket,
+  faEllipsis,
+  faEllipsisVertical,
+  faScrewdriver,
+  faCoins,
+  faBagShopping
+} from "@fortawesome/free-solid-svg-icons";
+import { faComment as frComment } from "@fortawesome/free-regular-svg-icons";
+
+const iconSet = {
+  tshirt: faShirt,
+  userGroup: faUserGroup,
+  package: faBoxOpen,
+  moneyBag: faSackDollar,
+  sqrs: faTableCellsLarge,
+  file: faFileLines,
+  shop: faShop,
+  magni: faMagnifyingGlass,
+  moon: faMoon,
+  sun: faSun,
+  comment: frComment,
+  bell: faBell,
+  goout: faRightFromBracket,
+  menuH: faEllipsis,
+  menuV: faEllipsisVertical,
+  tools: faScrewdriver,
+  coins: faCoins,
+  shoopingbag: faBagShopping
+}
+
+type iconSetKeys = keyof typeof iconSet;
+
+export { iconSet, type iconSetKeys }
